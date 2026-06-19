87de78a40df67a1dd92216ff2458e90aa848e9d1
Ardhi <ardhichoiruddin@yandex.com>
2026-06-20 01:25:13 +0700

    feat: add prerender setting to contact page server load

--- src/routes/[[preview=preview]]/contact/+page.server.ts
+++ src/routes/[[preview=preview]]/contact/+page.server.ts
@@ -2,6 +2,8 @@ import type { PageServerLoad } from './$types';
 import { createClient } from '$lib/prismicio';
 import { env } from '$env/dynamic/private';
 
+export const prerender = false;
+
 export const load: PageServerLoad = async ({ fetch, cookies, setHeaders }) => {
 	const PRISMIC_ACCESS_TOKEN = env.PRISMIC_ACCESS_TOKEN;
 	const client = createClient({ fetch, cookies, accessToken: PRISMIC_ACCESS_TOKEN });
